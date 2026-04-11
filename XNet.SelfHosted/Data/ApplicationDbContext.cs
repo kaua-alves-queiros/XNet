d49a6753b9c778ff756a6495b726a13f9c58d026
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2026-04-11 00:36:29 -0400

    feat(dashboard): implement real-time synchronization metrics and visualization overhaul

--- XNet.SelfHosted/Data/ApplicationDbContext.cs
+++ XNet.SelfHosted/Data/ApplicationDbContext.cs
@@ -7,4 +7,18 @@ namespace XNet.SelfHosted.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
     public DbSet<Organization> Organizations { get; set; }
+    public DbSet<Device> Devices { get; set; }
+    public DbSet<LogEntry> Logs { get; set; }
+    
+    // Terminal
+    public DbSet<TerminalSnippet> TerminalSnippets { get; set; }
+    public DbSet<TerminalLog> TerminalLogs { get; set; }
+    
+    // NetBox
+    public DbSet<NetBoxSite> NetBoxSites { get; set; }
+    public DbSet<NetBoxVLANGroup> NetBoxVLANDroups { get; set; }
+    public DbSet<NetBoxVLAN> NetBoxVLANs { get; set; }
+    public DbSet<NetBoxPrefix> NetBoxPrefixes { get; set; }
+    public DbSet<NetBoxDevice> NetBoxDevices { get; set; }
+    public DbSet<NetBoxIP> NetBoxIPs { get; set; }
 }
--- XNet.SelfHosted/Data/Migrations/20260410194422_AddDevicesAndLogs.Designer.cs
+++ XNet.SelfHosted/Data/Migrations/20260410194422_AddDevicesAndLogs.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using XNet.SelfHosted.Data;
 
@@ -10,9 +11,11 @@ using XNet.SelfHosted.Data;
 namespace XNet.SelfHosted.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260410194422_AddDevicesAndLogs")]
+    partial class AddDevicesAndLogs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.5");
